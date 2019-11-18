ba30b2916ad72c3b0e9c5c755cf9ed09564bc85f
Michael Chen <michaelchengaming@gmail.com>
2019-11-17 17:23:46 -0800

    clean up code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector('#new-toy-btn');
+const toyForm = document.querySelector('.container');
+let addToy = false;
 
 // YOUR CODE HERE
 
@@ -8,26 +8,26 @@ addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = 'block';
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = 'none';
   }
 })
 
 // OR HERE!
-  const toyCollection = document.getElementById("toy-collection")
-  const nameInput = document.querySelector("body > div.container > form > input:nth-child(2)")
-  const imgURL = document.querySelector("body > div.container > form > input:nth-child(4)")
+  const toyCollection = document.getElementById("toy-collection");
+  const nameInput = document.querySelector("body > div.container > form > input:nth-child(2)");
+  const imgURL = document.querySelector("body > div.container > form > input:nth-child(4)");
 
 // wait for page to load
 document.addEventListener("DOMContentLoaded", (e) => {
   fetchToys();
-  submitNewToy()
+  submitNewToy();
 });
 
 // fetch toys
 async function fetchToys(){
-  let res = await fetch("http://localhost:3000/toys")
+  let res = await fetch("http://localhost:3000/toys");
   let jsonObj = await res.json();
   jsonObj.forEach(toy => buildToys(toy));
 };
@@ -35,45 +35,43 @@ async function fetchToys(){
 // build toys
 function buildToys(toy){
   const toyCollection = document.getElementById("toy-collection");
-    const div = document.createElement("div")
-    div.className = "card";
-    const name = document.createElement("h2")
-    name.innerText = toy.name;
-    const image = document.createElement("img")
-    image.src = toy.image;
-    image.className = "toy-avatar";
-    const p = document.createElement("p")
-    p.innerHTML = `${toy.likes} Likes`
-    const button = document.createElement("button")
-    button.className = "like-btn"
-    button.innerText = "Like <3"
+  const div = document.createElement("div");
+  div.className = "card";
+  const name = document.createElement("h2");
+  name.innerText = toy.name;
+  const image = document.createElement("img");
+  image.src = toy.image;
+  image.className = "toy-avatar";
+  const p = document.createElement("p");
+  p.innerHTML = `${toy.likes} Likes`;
+  const button = document.createElement("button");
+  button.className = "like-btn";
+  button.innerText = "Like <3";
 
-    div.append(name, image, p, button)
-    toyCollection.appendChild(div);
+  div.append(name, image, p, button);
+  toyCollection.appendChild(div);
 
-    button.addEventListener("click", (e) => {
-      toy.likes += 1;
-      p.innerText = toy.likes + " Likes";
-      fetch(`http://localhost:3000/toys/${toy.id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({
-          likes: toy.likes
-        }) // add then stuff yadadadaa
-      })
-    });
-    
+  button.addEventListener("click", (e) => {
+    toy.likes += 1;
+    p.innerText = toy.likes + " Likes";
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        likes: toy.likes
+      }) // add then stuff yadadadaa
+    })
+  });
 };
 
 
 function submitNewToy(){
-  const submitBtn = document.querySelector("input.submit")
-  // console.log(submitBtn)
+  const submitBtn = document.querySelector("input.submit");
   submitBtn.addEventListener("click", (e) => {
-    e.preventDefault()
+    e.preventDefault();
     fetch('http://localhost:3000/toys', {
       method: "POST",
       headers: {
@@ -91,5 +89,4 @@ function submitNewToy(){
     .catch(err => console.log(err.message));
     toyForm.style.display = 'none';
   })
-  
 };
