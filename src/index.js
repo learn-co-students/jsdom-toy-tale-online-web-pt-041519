d798f2f5e23eafc2e4c4ee0cae20e1b642f6edf2
Dan Sullivan <dansullivan19@gmail.com>
2019-12-09 22:42:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ addBtn.addEventListener('click', () => {
 function fetchToys() {
   return fetch('http://localhost:3000/toys')
   .then( response => response.json() )
-  // .then( (toys) => renderToys(toys))
-  .then( toys => renderCards(toys))
-}
+  .then( toys  => renderCards(toys) )
+} // end of fetchToys
+
 
 function renderCards(toys) {
   const collection = document.getElementById("toy-collection")
@@ -40,24 +40,57 @@ function renderCards(toys) {
     img.height = 200
     cardDiv.appendChild(img)
 
+    let likes = toy.likes
+    cardDiv.setAttribute("likes", likes)
     let p = document.createElement('p')
-    p.innerHTML = `${toy.likes} Likes`
+    p.innerHTML = `${likes} Likes`
     cardDiv.appendChild(p)
 
     let btn = document.createElement('button')
     btn.classList.add("like-btn")
     btn.innerHTML = "Like <3"
+    btn.id = toy.id
+    // btn.addEventListener('click', (e) => console.log(e.target.id))
+    btn.addEventListener('click', incrementLikes)
     cardDiv.appendChild(btn)
-
     collection.appendChild(cardDiv)
+
   }
 
 }
 
-
 const submit = document.querySelector('[name="submit"]');
 submit.addEventListener('click', postNewToy);
  
+
+function incrementLikes(event) {
+ 
+  const likes = parseInt(event.target.parentElement.getAttribute("likes"))
+  const id = event.target.id
+
+  let formData = {
+    likes: likes + 1
+  };
+
+  let configObj = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+
+var element = event.target
+  fetch(`http://localhost:3000/toys/${id}`, configObj)
+  .then( response => response.json() )
+  .then( json => {
+      let card = document.getElementById(json.id)
+      card.previousSibling.innerHTML = json.likes + " Likes"
+    }
+  )
+}
 
 function postNewToy() {
  
@@ -69,12 +102,11 @@ function postNewToy() {
   let formData = {
     name: name,
     image: image,
-    likes: 0
+    likes: 0,
   };
 
   let configObj = {
     method: "POST",
-    mode: 'cors',
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
@@ -82,45 +114,16 @@ function postNewToy() {
     body: JSON.stringify(formData)
   };
 
-  return fetch('http://localhost:3000/toys', configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
+  // doesn't need to do anything to render new card after this fetch post.  the page
+  // just updates automatically
+  fetch('http://localhost:3000/toys', configObj)
   // .then( response => response.json() )
-  // .then ( object => alert(object))
-  // .then( toy => renderNewCard(toy) )
- // postNewToy end
-})}
+  // .then ( json => alert(json) )
 
-function renderNewCard(toy)  {
-  const collection = document.getElementById("toy-collection")
-    let cardDiv = document.createElement('div')
-    cardDiv.classList.add('card')
-    
-    let h2 = document.createElement('h2')
-    h2.innerHTML = `${toy.name}`
-    cardDiv.appendChild(h2)
-
-    let img = document.createElement('img')
-    img.classList.add("toy-avatar")
-    img.src = toy.image
-    img.width = 200
-    img.height = 200
-    cardDiv.appendChild(img)
-
-    let p = document.createElement('p')
-    p.innerHTML = `${toy.likes} Likes`
-    cardDiv.appendChild(p)
-
-    let btn = document.createElement('button')
-    btn.classList.add("like-btn")
-    btn.innerHTML = "Like <3"
-    cardDiv.appendChild(btn)
-
-    collection.appendChild(cardDiv)
 }
 
 
+
 fetchToys()
+
+
